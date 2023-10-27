7c59f709c3003d9b6afba21d3a8478340c933b82
FabioAVSilva <fabio.silva99@hotmail.com>
2023-10-27 20:31:01 +0100

    Initial GAS implementation: -PlayerState class -AttributeSet class -AbilitySystemComponent class

---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Fabio Silva
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
---
+++ Source/Aura/Private/Player/AuraPlayerState.cpp
@@ -0,0 +1,9 @@
+// Copyright Fabio Silva
+
+
+#include "Player/AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	NetUpdateFrequency = 100;
+}
