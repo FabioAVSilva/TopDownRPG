8dfb54571d08bf17d6e0d5f4894d50a998056a84
FabioAVSilva <fabio.silva99@hotmail.com>
2023-11-19 21:21:19 +0000

    Initialization of primary attributes using a Gameplay Effect

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -58,5 +58,6 @@ void AAuraCharacter::InitAbilityActorInfo()
 		
 	}
 
+	InitializePrimaryAttributes();
 	
 }
--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -4,7 +4,6 @@
 #include "Character/AuraCharacterBase.h"
 
 #include "AbilitySystemComponent.h"
-#include "Player/AuraPlayerState.h"
 
 AAuraCharacterBase::AAuraCharacterBase()
 {
@@ -30,5 +29,14 @@ void AAuraCharacterBase::BeginPlay()
 
 void AAuraCharacterBase::InitAbilityActorInfo()
 {
+}
+
+void AAuraCharacterBase::InitializePrimaryAttributes() const
+{
+	check(IsValid(GetAbilitySystemComponent()));
+	check(DefaultPrimaryAttributes);
+	const FGameplayEffectContextHandle ContextHandle = GetAbilitySystemComponent()->MakeEffectContext();
+	const FGameplayEffectSpecHandle SpecHandle = GetAbilitySystemComponent()->MakeOutgoingSpec(DefaultPrimaryAttributes, 1, ContextHandle);
+	GetAbilitySystemComponent()->ApplyGameplayEffectSpecToTarget(*SpecHandle.Data.Get(), GetAbilitySystemComponent());
 }
 
