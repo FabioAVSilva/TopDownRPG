12360c7ef9f583cc07c1af0a985b49621289b758
FabioAVSilva <fabio.silva99@hotmail.com>
2023-12-26 12:33:47 +0000

    Created AuraInputConfig class to store an array of FAuraInputAction, which has an InputAction and a GameplayTag, to later get an InputAction based on a given GameplayTag; Added GameplayTags for each Input Action; Created corresponding Input Actions and associated them in DA_AuraInputConfig BP;

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -28,6 +28,14 @@ void FAuraGameplayTags::InitializeNativeGameplayTags()
 	// Gameplay Tags for Vital Attributes
 	GameplayTags.Attributes_Vital_Health = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Vital.Health"), FString("Current amount of Health Points"));
 	GameplayTags.Attributes_Vital_Mana = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attributes.Vital.Mana"), FString("Current amount of Mana Points"));
+
+	// Gameplay Tags for Input Actions
+	GameplayTags.InputTag_LMB = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.LMB"), FString("Input Tag for Left Mouse Button"));
+	GameplayTags.InputTag_RMB = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.RMB"), FString("Input Tag for Right Mouse Button"));
+	GameplayTags.InputTag_1 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.1"), FString("Input Tag for 1 key"));
+	GameplayTags.InputTag_2 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.2"), FString("Input Tag for 2 key"));
+	GameplayTags.InputTag_3 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.3"), FString("Input Tag for 3 key"));
+	GameplayTags.InputTag_4 = UGameplayTagsManager::Get().AddNativeGameplayTag(FName("InputTag.4"), FString("Input Tag for 4 key"));
 	
 	
 }
