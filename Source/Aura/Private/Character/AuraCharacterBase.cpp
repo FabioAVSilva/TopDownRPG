351c263a81b88b459c7f49789bae5a2794c27064
FabioAVSilva <fabio.silva99@hotmail.com>
2024-01-05 16:35:01 +0000

    Implemented projectile having a Damage Effect so it subtracts health from the actor it collides with;

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -15,6 +15,7 @@ AAuraCharacterBase::AAuraCharacterBase()
 
 	// Set collision channel responses
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	GetCapsuleComponent()->SetGenerateOverlapEvents(false);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
 	GetMesh()->SetCollisionResponseToChannel(ECC_Projectile, ECR_Overlap);
 	GetMesh()->SetGenerateOverlapEvents(true);
